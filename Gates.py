80d91242827f6a47d1abedb88f05489bf87ab1a6
Łukasz Klępka <lukasz0klepka@gmail.com>
2023-11-09 17:02:03 +0100

    Add new view without callbacks

--- Gates.py
+++ Gates.py
@@ -1,26 +1,47 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QTextEdit, QLabel
 import sys
-from process.loader import FileLoader
+from pages.loader import FileLoader
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Moja Aplikacja PyQt")
+        self.setWindowTitle("Gates Manager")
         self.setGeometry(100, 100, 800, 600)
-        self.init_tabs()
+        self.init_ui()
 
-    def init_tabs(self):
-        # Inicjalizacja QTabWidget
+    def init_ui(self):
+        
+        main_layout = QVBoxLayout()
+
+        # Inicialization of tabs
         self.tabs = QTabWidget()
-        self.setCentralWidget(self.tabs)
+        main_layout.addWidget(self.tabs)
 
-        # Dodanie głównego widoku jako pierwszej zakładki
+        # Add main view as a start window
         self.main_view = QWidget()
         self.tabs.addTab(self.main_view, "Main")
 
-        # Dodanie loader_view jako drugiej zakładki
-        self.loader_view = FileLoader()
+        # Add Loader tab
+        self.loader_view = FileLoader(self.update_console)  
         self.tabs.addTab(self.loader_view, "Loader")
+
+        # Add message log
+        self.console_label = QLabel("Message log:")
+        main_layout.addWidget(self.console_label)
+
+        self.console = QTextEdit()
+        self.console.setReadOnly(True)
+        self.console.setFixedHeight(100)
+        main_layout.addWidget(self.console)
+
+        # Set main widget
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
+
+    def update_console(self, message):
+        # Update console to all tabs
+        self.console.append(message)
 
 def main():
     app = QApplication(sys.argv)
