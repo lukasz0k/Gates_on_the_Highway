56c8256c37e68e21b30dea1349e1bbf1eeedc84d
Łukasz Klępka <lukasz0klepka@gmail.com>
2023-11-10 17:33:49 +0100

    Update error handling in log loads

--- process/loadAndValidate.py
+++ process/loadAndValidate.py
@@ -2,55 +2,55 @@ import os
 
 class LoadInputsData:
     def loadLogs(self, file_path):
-        # Verify the file extension is .txt
+        # Check if file exists
+        if not os.path.exists(file_path):
+            return "File not found"
+
+        # Check if the file extension is .txt
         if not file_path.lower().endswith('.txt'):
-            raise ValueError("File is not a .txt format")
-        
+            return "File is not a .txt format"
+
         # Check if the file is empty
         if os.stat(file_path).st_size == 0:
-            raise ValueError("File is empty")
+            return "File is empty, no logs loaded"
 
-        # Try to open and read the file
         try:
             with open(file_path, 'r') as file:
-                for line_number, line in enumerate(file, start=1):
-                    if not line.strip():  # Skip empty lines
-                        continue
-                    columns = line.split()  # Assuming whitespace delimiter
+                lines = file.readlines()
 
-                    if len(columns) < 4:  # Check if there are at least 4 columns
-                        raise ValueError(f"Line {line_number}: Not enough columns")
-                    
-                for line_number, line in enumerate(file, start=1):
-                    columns = line.split()  # Assuming whitespace delimiter
-                    
-                    # Verify the first column is a float
+                if not lines:
+                    return "File is empty, no logs loaded"
+
+                for line_number, line in enumerate(lines, start=1):
+                    columns = line.strip().split()
+
+                    if len(columns) < 4:
+                        return f"Invalid data in file, file need to have 4 columns"
+
+                    # Check first column is a float
                     try:
                         float(columns[0])
                     except ValueError:
-                        raise ValueError(f"Line {line_number}: First column is not a float")
+                        return f"Invalid data format, first column must be a number"
 
-                    # Verify the rest of the columns are text
-                    for col in columns[1:]:
+                    # Check remaining columns are text
+                    for i, col in enumerate(columns[1:], start=2):
                         if not col.isalpha():
-                            raise ValueError(f"Line {line_number}: Column is not text")
+                            return f"Invalid data format, column {i} must be a text"
 
-                    # Verify the third column is one of the specified values
+                    # Specific value checks for columns 3 and 4
                     if columns[2] not in ["240W", "250W", "260W", "270W", "270E", "260E", "250E", "240E"]:
-                        raise ValueError(f"Line {line_number}: Third column has invalid value")
-
-                    # Verify the fourth column is one of the specified values
+                        return "Third column has invalid value"
                     if columns[3] not in ["ENTRY", "ON ROAD", "EXIT"]:
-                        raise ValueError(f"Line {line_number}: Fourth column has invalid value")
+                        return "Fourth column has invalid value"
 
         except FileNotFoundError:
-            raise FileNotFoundError("File does not exist")
-        
+            return "File not found"
         except Exception as e:
-            # Handle any other exception that might occur
-            raise e
+            # Generic catch-all for any other exception
+            return f"An error occurred: {e}"
 
-        return True  # Or the data, depending on requirements
+        return "File loaded"  # Return this message if all checks pass
     
     def loadThreads(self, file_path):
         # Check if the file extension is .txt or .csv
